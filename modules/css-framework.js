6dce97e228978f0814ed850f301398dd545bad3e
JOSE MANUEL MARTINEZ LAINEZ - E437892 <E437892@inetpsa.com>
2018-03-16 12:37:42 +0100

    added locale dependencie

--- modules/css-framework.js
+++ modules/css-framework.js
@@ -28,8 +28,8 @@ define([
 
     var w20CSSFramework = angular.module('w20CSSFramework', ['ui.bootstrap', 'w20CoreEnv', 'w20CoreSecurity', 'w20CoreCulture']);
 
-    w20CSSFramework.run(['EventService', 'CultureService', 'uibDatepickerConfig', 'uibDatepickerPopupConfig',
-        function (eventService, cultureService, datepickerConfig, datepickerPopupConfig) {
+    w20CSSFramework.run(['EventService', '$locale', 'CultureService', 'uibDatepickerConfig', 'uibDatepickerPopupConfig',
+        function (eventService, $locale, cultureService, datepickerConfig, datepickerPopupConfig) {
 
             datepickerConfig.formatDay = 'dd';
             datepickerConfig.formatMonth = 'MMMM';
